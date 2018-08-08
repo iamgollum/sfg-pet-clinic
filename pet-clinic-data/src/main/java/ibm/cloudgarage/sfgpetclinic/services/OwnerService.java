8ea7bc7faaaf97a35cf54344411312c1a9374d9e
Aaron Tobias <tobias613.aaron@ibm.com>
2018-08-08 10:37:11 -0400

    Refactored models to common crud interface. closes #11

--- pet-clinic-data/src/main/java/ibm/cloudgarage/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/ibm/cloudgarage/sfgpetclinic/services/OwnerService.java
@@ -1,16 +1,9 @@
 package ibm.cloudgarage.sfgpetclinic.services;
 
 import ibm.cloudgarage.sfgpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Set;
 
-public interface OwnerService {
-
+public interface OwnerService extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/ibm/cloudgarage/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/ibm/cloudgarage/sfgpetclinic/services/VetService.java
@@ -2,13 +2,4 @@ package ibm.cloudgarage.sfgpetclinic.services;
 
 import ibm.cloudgarage.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findByid(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
-}
+public interface VetService extends CrudService<Vet, Long> { }
